fddbb315c634c3f8dfef38c1b93a51d703a07320
anishbista92 <anishbista9235@gmail.com>
2024-02-19 17:33:12 +0545

    added api for users list and details

--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import PostList, PostDetail
+from .views import PostList, PostDetail, UserList, UserDetail
 
 urlpatterns = [
     path("<int:pk>/", PostDetail.as_view(), name="post_detail"),
     path("", PostList.as_view(), name="post_list"),
+    path("users/", UserList.as_view(), name="users"),
+    path("users/<int:pk>/", UserDetail.as_view(), name="user_detail"),
 ]
