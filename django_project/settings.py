9e83f278e7778b023f989362bd3354a2d2074e5d
anishbista92 <anishbista9235@gmail.com>
2024-02-20 08:56:04 +0545

    Finished api for django

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-glqnza%01z+@@6#ci)j3*5z8!@__re*4r5i2lt)t_^vi425g#n"
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool("DEBUG", default=False)
 
 ALLOWED_HOSTS = []
 
